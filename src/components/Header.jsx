ba75d8601e82175ce60cf62a9c4561335935e13c
Sonwabiso <sonwabisolebokgang@gmail.com>
2024-06-17 03:59:39 +0200

    red color theme for header and footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import headerimg from '../assets/images/Podpeek-logo.png'
 
 const Header = () => {
   return (
-    <header className="bg-blue-500 text-white p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold"><Link to="/">Podcast App</Link></h1>
-        <nav>
-          <Link to="/" className="mr-4">Home</Link>
+    <header className="bg-white text-sm text-[#e60000]">
+      <div className="w-full py-4 flex">
+        <div className="px-4"><Link to="/"><img src={headerimg} alt="podpeek logo image" className="mx-1 h-30 w-28" /></Link>
+      </div>
+      <div className="w-full pt-3">
+        <div className='py-1'>
+          <nav className='flex space-x-10'>
+            <Link to="/">About</Link>
+            <Link to="/">Contact</Link>
+          </nav>
+          </div>
+          <hr className="w-full h-0.5 bg-red-600"/>
+        <div className="py-1">
+          <nav>
+            <Link>Subscribe to our newsletter</Link>
+          </nav>
+          </div>
+      </div>
+        
+        {/* <nav>
+          <Link to="/" className="">Home</Link>
           <Link to="/login">Login</Link>
-        </nav>
+        </nav> */}
       </div>
     </header>
   );

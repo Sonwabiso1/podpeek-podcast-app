870b82282a1cef396ec822ed3e7e79dd85a4dd6f
Sonwabiso <sonwabisolebokgang@gmail.com>
2024-06-20 22:28:44 +0200

    randomize carousel

--- src/components/PodcastCarousel.jsx
+++ src/components/PodcastCarousel.jsx
@@ -1,8 +1,15 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const PodcastCarousel = ({ podcasts }) => {
   const carouselRef = useRef(null);
+  const [randomPodcasts, setRandomPodcasts] = useState([]);
+
+  useEffect(() => {
+    // Shuffle podcasts and select 7
+    const shuffledPodcasts = podcasts.sort(() => 0.5 - Math.random());
+    setRandomPodcasts(shuffledPodcasts.slice(0, 7));
+  }, [podcasts]);
 
   const scrollLeft = () => {
     carouselRef.current.scrollBy({ left: -300, behavior: 'smooth' });
@@ -15,19 +22,19 @@ const PodcastCarousel = ({ podcasts }) => {
   return (
     <div className="pl-[85px] ml-7 pr-[64px] mt-8 text-[#e60000]">
       <h2 className="text-2xl font-bold mb-4">Featured Podcasts</h2>
-      <div className="relative flex items-center">
+      <div className="flex items-center">
         <button onClick={scrollLeft} className="absolute left-0 ml-2 z-10 bg-gray-200 p-2 rounded-full">
           &lt;
         </button>
         <div ref={carouselRef} className="flex overflow-x-auto space-x-4 scrollbar-hide">
-          {podcasts.map((podcast) => (
+          {randomPodcasts.map((podcast) => (
             <div key={podcast.id} className="min-w-[300px] border border-[#e60000] rounded-lg p-4 flex-shrink-0 flex h-48 w-full">
-              <Link to={`/podcast/${podcast.id}`} className="w-full">
-                <img src={podcast.image} alt="podcast image" className="h-40 w-40 object-cover mb-2" />
+              <Link to={`/podcast/${podcast.id}`} className="w-1/3">
+                <img src={podcast.image} alt="podcast image" className="h-full w-full object-cover mb-2 rounded-lg" />
               </Link>
-              <div className="ml-4">
+              <div className="w-2/3 ml-4 overflow-hidden">
                 <h3 className="text-xl font-bold">{podcast.title}</h3>
-                <p className="text-sm">{podcast.description}</p>
+                <p className="text-sm overflow-y-auto max-h-36 scrollbar-hide">{podcast.description}</p>
               </div>
             </div>
           ))}
